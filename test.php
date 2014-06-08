da9e38687b29874a45cf592ced718e6b2df36a31
HarryStevens <harryjosephstevens@gmail.com>
2014-06-08 17:41:01 -0400

    Creating test file to save FB data

--- test.php
+++ test.php
@@ -1,8 +1,10 @@
-<html>
-	<head>
-		<title>PHP Test</title>
-	</head>
-	<body>
-		<?php echo '<p>Hello World</p>'; ?>
-	</body>
-</html>
+<?php
+    require 'client/facebook.php';
+	
+	$app_id = '469565369845051';
+	$secret = 'a51bff788776dc2766534e533ced0223';
+	$app_url = 'http://jfeed.co/';
+
+    
+    
+?>
